1619da28913dc6ea60395f2576a2d51f8e5f118b
Paulo Henrique Silva Marcusso <paulo_marcusso@hst.br>
2021-07-10 21:16:04 -0300

    configurando conexao com banco de dados

--- src/main/java/br/com/alura/service/AgendamentoEmailService.java
+++ src/main/java/br/com/alura/service/AgendamentoEmailService.java
@@ -3,11 +3,18 @@ package br.com.alura.service;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.alura.dao.AgendamentoEmailDAO;
+import br.com.alura.domain.AgendamentoEmail;
 
 @Stateless
 public class AgendamentoEmailService {
+	
+	@Inject
+	private AgendamentoEmailDAO agendamentoEmailDAO;
 
-	public List<String> listar(){
-		return List.of("paulo@hst.com.br", "pamela@midway.com.br");
+	public List<AgendamentoEmail> listar(){
+		return agendamentoEmailDAO.listar();
 	}
 }
