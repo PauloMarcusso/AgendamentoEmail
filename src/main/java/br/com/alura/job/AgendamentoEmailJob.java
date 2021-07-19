e45b4c7592e5c1dedfe213cb7d021cc9882220a4
Paulo Henrique Silva Marcusso <paulo_marcusso@hst.br>
2021-07-19 10:40:41 -0300

    trabalhando com filas jms

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -2,10 +2,14 @@ package br.com.alura.job;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import br.com.alura.domain.AgendamentoEmail;
 import br.com.alura.service.AgendamentoEmailService;
@@ -16,11 +20,18 @@ public class AgendamentoEmailJob {
 	@Inject
 	private AgendamentoEmailService agendamentoEmailService;
 	
+	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+	private JMSContext context;
+	
+	@Resource(mappedName = "java:/jms/queue/EmailQueue")
+	private Queue queue;
+	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public void enviarEmail() {
 		List<AgendamentoEmail> listaEmailsNaoAgendados = agendamentoEmailService.listarPorNaoAgendado();
 		listaEmailsNaoAgendados.forEach(emailNaoAgendado -> {
-			agendamentoEmailService.enviarEmail(emailNaoAgendado);
+			context.createProducer().send(queue, emailNaoAgendado);
 			agendamentoEmailService.alterar(emailNaoAgendado);
 		});
 		
